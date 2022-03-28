3b3a875f5d54ecddc3a2147627b3a8ba9972feb7
Kento Nakayama <kento427@outlook.com>
2022-03-28 11:15:08 +0900

    ready for hyper-v gen2

---
+++ vhd2diskDirect_gen2.ps1
@@ -0,0 +1,31 @@
+#sysprep してない vhd を Disk に直接アップロードする
+# https://docs.microsoft.com/ja-jp/azure/virtual-machines/windows/disks-upload-vhd-to-managed-disk-powershell
+
+######## 変更するパラメータ
+
+$souceVHDfile = "./XXXX.vhd"
+$diskname = "XXXXos"
+$Region = "japaneast"
+$ResourceGroup = "XXXX"
+
+########
+
+$vhdSizeBytes = (Get-Item $souceVHDfile).length
+
+$diskconfig = New-AzDiskConfig -SkuName 'Standard_LRS' -OsType 'Windows' -UploadSizeInBytes $vhdSizeBytes -Location $Region -CreateOption 'Upload' -HyperVGeneration V2
+
+New-AzDisk -ResourceGroupName $ResourceGroup -DiskName $diskname -Disk $diskconfig 
+
+
+$diskSas = Grant-AzDiskAccess -ResourceGroupName $ResourceGroup -DiskName $diskname -DurationInSecond 86400 -Access 'Write'
+
+$disk = Get-AzDisk -ResourceGroupName $ResourceGroup -DiskName $diskname
+
+./AzCopy copy $souceVHDfile $diskSas.AccessSAS --blob-type PageBlob
+
+Revoke-AzDiskAccess -ResourceGroupName $ResourceGroup -DiskName $diskname
+
+
+#ここまでが vhd -> Disk の作成
+
+
